3698fc17c2c3a0b36d22a95c5789c876d9be2fde
vishalsingh5126 <42098634+vishalsingh5126@users.noreply.github.com>
2018-08-08 12:30:33 +0530

    Script and content files added

---
+++ Scripts/modalform.js
@@ -0,0 +1,53 @@
+﻿//$(function () {
+//    $.ajaxSetup({ cache: false });
+
+//    $("a[data-modal]").on("click", function (e) {
+//        
+//        $('#myModalContent').load(this.href, function () {
+//            $('#myModal').modal({
+//                keyboard: true
+//            }, 'show');
+//            bindForm(this);
+//        });
+//        return false;
+//    });
+//});
+
+//function bindForm(dialog) {
+//    $('form', dialog).submit(function () {
+//        $.ajax({
+//            url: this.action,
+//            type: this.method,
+//            data: $(this).serialize(),
+//            success: function (result) {
+//                if (result.success) {
+//                    $('#myModal').modal('hide');
+//                    loadDataTable(); //  Load data from the server and place the returned HTML into the matched element
+//                } else {
+//                    $('#myModalContent').html(result);
+//                    bindForm(dialog);
+//                }
+//            }
+//        });
+//        return false;
+//    });
+
+//    function loadDataTable() {
+//        var selProjectCode = $('#projectCode').val();
+
+//        $.ajax({
+//            cache: false,
+//            url: "/QuickLink/SearchQuickLinks/",
+//            type: "GET",
+//            data: 'projectCode=' + selProjectCode,
+//            success: function (result) {
+//                $('#searchResult').html(result);
+//            },
+//            error: function (result) {
+//                console.log("Hey!, there's an error here");
+//            }
+//        });
+//    }
+//            
+
+//}
